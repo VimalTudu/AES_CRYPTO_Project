f66184c39b5b2d9ceb06323232772fda1a548f30
VimalTudu <89855569+VimalTudu@users.noreply.github.com>
2022-01-17 15:17:24 +0530

    Implemented function of about and help functions

--- aesexample/MyFrame.java
+++ aesexample/MyFrame.java
@@ -14,6 +14,10 @@ import javax.swing.ImageIcon;
 import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 
+//New module added
+import javax.swing.JOptionPane;//new module import
+import javax.swing.JTextArea;//new module import
+
 import java.awt.image.BufferedImage;
 import java.awt.event.KeyEvent;
 import java.awt.event.ActionListener;
@@ -35,6 +39,8 @@ public class MyFrame extends JFrame implements ActionListener {
 
     JMenu fileMenu, jcryptMenu, infoMenu;
     JMenuItem openFileItem, saveItem, exitItem;
+    
+    JMenuItem about,help;
     
     ImageIcon openFileIcon, saveItemIcon, exitIcon;
     ImageIcon backgroundImage;
@@ -77,16 +83,26 @@ public class MyFrame extends JFrame implements ActionListener {
         openFileItem = new JMenuItem("Open");
         saveItem = new JMenuItem("Save");
         exitItem = new JMenuItem("Exit");
-
+        
+        //New Menu Item added
+        //Menu-Items for infoMenu
+        about =new JMenuItem("About");
+        help=new JMenuItem("Help");
+        
         // Icons for File-Menu-Itenms
         openFileIcon = new ImageIcon(openFileIconPath);
         saveItemIcon = new ImageIcon(saveFileIconPath);
         exitIcon = new ImageIcon(exitFileIconPath);
+       
 
         // actionListeners
         openFileItem.addActionListener(this);
         saveItem.addActionListener(this);
         exitItem.addActionListener(this);
+        
+        //action Listners for infoMenu items added
+        about.addActionListener(this);
+        help.addActionListener(this);
 
         // set icons for file Menu
         openFileItem.setIcon(openFileIcon);
@@ -107,6 +123,10 @@ public class MyFrame extends JFrame implements ActionListener {
         fileMenu.add(openFileItem);
         fileMenu.add(saveItem);
         fileMenu.add(exitItem);
+        
+        //add menu-items to info Menu
+        infoMenu.add(about);
+        infoMenu.add(help);
 
         // add menu to Menu Bar
         menuBar.add(fileMenu);
@@ -153,6 +173,40 @@ public class MyFrame extends JFrame implements ActionListener {
                 System.out.println(file);
             }
         }
+        
+        //Code to add
+        /*
+        //About
+        if (action.getSource()== about){
+
+            String message="\tPROJECT WORK\n"+"\tCOMPUTER SCIENCE AND ENGINEERING\n\n"+"Description:This is project on image encryption and decryption\n"+
+            "using AES algorithm\n\n\n"
+            +"Project By:\n"+"NAME:SAPAN RAVIDAS\t\t  NAME:VIMAL TUDU\n"+"ROLL NO:1809027 \t\t\t  ROLL NO:1809033\n"+"BRANCH:CSE \t\t  BRANCH:CSE"+
+            "\n\nSubmitted To:PROF TAPAN NAYAK";
+        
+            JOptionPane.showMessageDialog(this, message, "About",JOptionPane.INFORMATION_MESSAGE);
+            
+         }
+        //Help
+        if(action.getSource()== help){
+             String message="This is project in which encryption and decryption of image is done."+"\nFile Menu:Provides fumction to open,save file and exit."+
+            "\nJEcrypt Menu:Provides functionality of encryption and decryption"+"\nInfo Menu:Provides information about author and how to use the app";
+            // System.out.println(message);
+            
+            JTextArea textArea=new JTextArea(10,10);
+        
+            textArea.setLineWrap(true);
+            textArea.setWrapStyleWord(true);
+            textArea.setText(message);
+            textArea.setEditable(false);
+
+            JScrollPane scrollPane=new JScrollPane(textArea);
+            //JOptionPane.showMessageDialog(this, message,"Help", JOptionPane.QUESTION_MESSAGE);
+            JOptionPane.showMessageDialog(this,scrollPane,"Help",JOptionPane.QUESTION_MESSAGE);
+            
+         }
+        */
+    
     }
 
     // ############################################################################# end of actionPerformed functions
